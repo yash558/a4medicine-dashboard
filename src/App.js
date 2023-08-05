61708fd0afd19bf52f76a12ee1e1175df42d6d55
yash sharma <76558099+yash558@users.noreply.github.com>
2023-08-05 19:41:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Route path="/dashboard/*" element={<Dashboard />} />
     <Route path="/auth/*" element={<Auth />} /> 
     <Route path="/dashboard/chart/:id" element={<ChartTopic/>} />
-    <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    <Route path="*" element={<Navigate to="/dashboard/charts" replace />} />
   </Routes>
   );
 }
